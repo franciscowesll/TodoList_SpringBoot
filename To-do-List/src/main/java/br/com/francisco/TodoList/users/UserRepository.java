d8e6d3b98383c6d5e57a25f7893844b9765747f5
Francisco Weslley <84885942+franciscowesll@users.noreply.github.com>
2023-10-15 15:59:37 -0300

    Delete To-do-List directory

--- To-do-List/src/main/java/br/com/francisco/TodoList/users/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.francisco.TodoList.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-public interface UserRepository  extends JpaRepository<UserModel, UUID>{
-    UserModel findByUsername(String username);
-    UserModel findByPassword(String password);
-}
